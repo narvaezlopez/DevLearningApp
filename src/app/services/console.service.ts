1b1b9e28ddb116f48c4c9edc50ea9c8cdf9ff3e2
Julian Narvaez <oscarnarvaez01@hotmail.com>
2020-06-11 00:27:32 -0500

    version console working

--- src/app/services/console.service.ts
+++ src/app/services/console.service.ts
@@ -9,9 +9,6 @@ import { Output } from '../models/output';
 export class ConsoleService {
 
   public url:string = 'http://localhost:3000/console';
-
-  
-
   constructor(private http:HttpClient) { }
 
   postExecute(){
@@ -24,15 +21,16 @@ export class ConsoleService {
     });
   }
 
-  postExecuteR():void{
+  postExecuteR(code:any,language:any):void{
     this.http.post(this.url,{
       "clientId":"db58b57ea8b695f03a3e02e88315ef87",
       "clientSecret":"1b5b4656297925113cf9965ae03c964fc87ae8051f07e6c2ad9f0ba35448e2f7",
-      "script":"<?php echo \"hello\"; ?>",
-      "language": "php",
+      "script":code,
+      "language": language,
       "versionIndex": "0"
     }).subscribe((response) => {
       console.log(response);
+      console.log(code);
   });;
   }
 }
