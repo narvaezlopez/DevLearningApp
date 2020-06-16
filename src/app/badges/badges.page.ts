0aac6fdb69cf07bc6d4a4ab608abf739a6768d8e
Karen Daniela Cuervo Cely <cuervo.cely.9@gmail.com>
2020-06-16 14:30:17 -0500

    list badges for user

--- src/app/badges/badges.page.ts
+++ src/app/badges/badges.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {BadgeUserService} from '../services/badge-user.service';
-import {BadgesService} from '../services/badges.service';
+import { BadgeUserService } from '../services/badge-user.service';
+import { BadgesService } from '../services/badges.service';
 
 @Component({
   selector: 'app-badges',
@@ -10,21 +10,26 @@ import {BadgesService} from '../services/badges.service';
 export class BadgesPage implements OnInit {
   badges_users: any[] = [];
   badges: any[] = [];
-  constructor(private badgeUserService:BadgeUserService, private badgeService:BadgesService) { }
+  constructor(private badgeUserService: BadgeUserService, private badgeService: BadgesService) { }
 
   //RDdHIsiP5ChIUiipTEYPyL4wuuH2
   ngOnInit() {
-    this.badgeUserService.getBadgeUserById('RDdHIsiP5ChIUiipTEYPyL4wuuH2').subscribe((badges_users) => {
+    this.badgeUserService.getBadgeUserByIdUser('RDdHIsiP5ChIUiipTEYPyL4wuuH2').subscribe((badges_users) => {
       this.badges_users = <any[]>badges_users;
       console.log(badges_users);
+
+      this.badges_users.forEach(element => {
+        this.badgeService.getBadgesById(element['badge']).subscribe((badges) => {
+          this.badges.push(badges);
+          console.log(badges);
+        });
+
+      });
+
+
     });
 
-    this.badges_users.forEach(element => {
-      this.badges.push(this.badgeService.getBadgesById(element['badge']))
-      console.log(this.badges);
-    });
 
-    
   }
 
 }
