d6c38b3b1bc7d3f6cc6f2e13dbe74d29107eadff
Julian Narvaez <oscarnarvaez01@hotmail.com>
2020-06-18 00:35:24 -0500

    Login component is changed by Login page, some style changes are applied and a form template is done

--- src/app/challenges/challenges.module.ts
+++ src/app/challenges/challenges.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+
 import { IonicModule } from '@ionic/angular';
+
 import { ChallengesPageRoutingModule } from './challenges-routing.module';
+
 import { ChallengesPage } from './challenges.page';
 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../../environments/environment';
-
 import { AngularFireAuthModule, AngularFireAuth } from '@angular/fire/auth';
+
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   imports: [
